5108171d47d5026de0d96af4a6f61d16f732fc0a
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-10-29 15:44:38 -0400

    mutiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,8 +67,18 @@ var fight = function(enemyName) {
   }
 };
 
+
+
+if (playerHealth > 0) {
+  // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+  window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+  
+
 for (var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
   enemyHealth = 50;
   fight(pickedEnemyName);
+  
+}
+
 }
